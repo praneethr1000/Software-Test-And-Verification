8cfa397558ee2548f0ebc4ceb391e585aaa52816
Praneeth <praneethr1000@gmail.com>
2022-07-28 10:36:07 -0700

    Attachment and export options added

--- bughound/bughound/settings.py
+++ bughound/bughound/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     # apps
     'bugtracker.apps.BugtrackerConfig',
     'django_filters',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- bughound/bugtracker/admin.py
+++ bughound/bugtracker/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 # Register your models here.
 from .models import Bugtracker
 
-admin.site.register(Bugtracker)
+# admin.site.register(Bugtracker)
+
+
+class BugResource(resources.ModelResource):
+    class Meta:
+        model = Bugtracker
+
+
+class BugAdmin(ImportExportModelAdmin):
+    resource_class = BugResource
+
+
+admin.site.register(Bugtracker, BugAdmin)
